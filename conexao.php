4b91fc8a27022cbd0fcf22b31ecc0a82894cfdaf
LazarisGabriel <gal.lazaris@hotmail.com>
2022-07-13 14:04:23 -0300

    teste commit
    teste

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $connection_string = 'host=localhost port=5432 dbname=PIN user=postgres password=1234';
+    $connection_string = 'host=localhost port=5432 dbname=PIN user=postgres password=123fsdfdsfds4';
     $conn = pg_connect($connection_string);
         echo 'Conexão realizada';
 
